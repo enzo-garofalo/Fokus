144bc3fa943af537dc8120396f864f0dbffeb759
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-07-27 10:24:48 -0300

    Switching images

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const focoBT = document.querySelector('.app__card-button--foco');
 const curtoBT = document.querySelector('.app__card-button--curto');
 const longoBT = document.querySelector('.app__card-button--longo');
 
-const timer = document.getElementById('timer');
-const image = document.getElementsByClassName('app__image');
-const title = document.getElementsByClassName('app__title');
+const timer = document.querySelector('#timer');
+const image = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
 
 const duracaoFoco = 1500; 
 const duracaoDescansoCurto = 300; 
@@ -13,12 +13,15 @@ const duracaoDescansoLongo = 900;
 
 focoBT.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    image.setAttribute('src', './imagens/foco.png')
 })
 
 curtoBT.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    image.setAttribute('src', './imagens/descanso-curto.png')
 })
 
 longoBT.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    image.setAttribute('src', './imagens/descanso-longo.png')
 })
