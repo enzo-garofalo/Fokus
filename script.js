44047d491fc2d22fe5a228ecbddebb91d2b1fb9c
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-07-27 10:14:34 -0300

    Eventos ...

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-const html = document.querySelector('html')
-const focoBT = document.querySelector('.app__card-button--foco')
-const curtoBT = document.querySelector('.app__card-button--curto')
-const longoBT = document.querySelector('.app__card-button--longo')
+const html = document.querySelector('html');
+const focoBT = document.querySelector('.app__card-button--foco');
+const curtoBT = document.querySelector('.app__card-button--curto');
+const longoBT = document.querySelector('.app__card-button--longo');
+
+const timer = document.getElementById('timer');
+const image = document.getElementsByClassName('app__image');
+const title = document.getElementsByClassName('app__title');
 
 focoBT.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
+    html.setAttribute('data-contexto', 'foco');
 })
 
 curtoBT.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    html.setAttribute('data-contexto', 'descanso-curto');
 })
 
 longoBT.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    html.setAttribute('data-contexto', 'descanso-longo');
 })
